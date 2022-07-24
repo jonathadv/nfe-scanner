bdeb688fcdd5b5ba885e55e89c63a959c02ae0e0
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2022-07-24 20:08:32 -0300

    add AVULSO and POTE as metric units

--- nfe_scanner/parsers/common.py
+++ nfe_scanner/parsers/common.py
@@ -15,7 +15,7 @@ def parse_decimal(value: str, decimal_separator: str = ",") -> Decimal:
 def parse_metric_unit(unit: str) -> MetricUnit:
     if unit == "KG":
         return MetricUnit.KG
-    if unit in ("UN", "EX"):
+    if unit in ("UN", "EX", "AVULSO", "POTE"):
         return MetricUnit.UNIT
     raise ValueError(f"value f'{unit}' is not valid unit.")
 
