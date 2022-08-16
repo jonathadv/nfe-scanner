387e4f7e3d14d925b232e3aceaa1295b95db615f
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2022-08-16 19:11:21 -0300

    add CAIXA as metric units

--- nfe_scanner/parsers/common.py
+++ nfe_scanner/parsers/common.py
@@ -15,7 +15,7 @@ def parse_decimal(value: str, decimal_separator: str = ",") -> Decimal:
 def parse_metric_unit(unit: str) -> MetricUnit:
     if unit == "KG":
         return MetricUnit.KG
-    if unit in ("UN", "EX", "AVULSO", "POTE"):
+    if unit in ("UN", "EX", "AVULSO", "POTE", "CAIXA"):
         return MetricUnit.UNIT
     raise ValueError(f"value f'{unit}' is not valid unit.")
 
